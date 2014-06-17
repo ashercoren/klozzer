ae45d378fc26d8842e2c04de133c326af5768767
Yehonathan Sharvit <viebel@gmail.com>
2014-06-17 15:03:26 +0300

    handle errors + 0.2.3

--- src/cljs/klozzer/core.cljs
+++ src/cljs/klozzer/core.cljs
@@ -18,32 +18,33 @@
   IFileSystem 
   (-write [this filename data]
     (let [c (chan)]
-      (!> fs.root.getFile
-          filename
-          (obj :create true)
-          (fn [file-entry]
-            (!> file-entry.createWriter
-                (fn [writer]
-                  (!> writer.write (js/Blob. #js [data] (obj :type "application/octet-binary")))
-                  (put! c :done))
-                (partial my-error-handler "createWriter" c))))
+      (go
+        (if-let [file-entry (<! (-file-entry this filename true))]
+          (!> file-entry.createWriter
+              (fn [writer]
+                (! writer.onerror (partial my-error-handler "writer.write" c))
+                (! writer.onwriteend #(put! c :done))
+                (!> writer.write (js/Blob. #js [data] (obj :type "application/octet-binary"))))
+              (partial my-error-handler "createWriter" c))
+          (do (.log js/console "Cannot -write to: " filename)
+              (close! c))))
       c))
-  (-file-entry [this filename]
+  (-file-entry [this filename create?]
     (let [c (chan)]
-      (!> fs.root.getFile filename (obj :create false) 
+      (!> fs.root.getFile filename (obj :create create?) 
           #(put! c %)
           (partial my-error-handler "fs.root.getFile" c #{"NotFoundError"}))
       c))
   (-file [this filename]
     (let [c (chan)]
       (go
-        (if-let [file-entry (<! (-file-entry this filename))]
+        (if-let [file-entry (<! (-file-entry this filename false))]
           (!> file-entry.file #(put! c %))
           (close! c)))
       c))
   (-url [this filename]
     (go 
-      (if-let [file-entry (<! (-file-entry this filename))]
+      (if-let [file-entry (<! (-file-entry this filename false))]
         (!> file-entry.toURL )
         "")))
   (-read [this filename format]
@@ -63,11 +64,12 @@
   (let [c (chan)]
     (!> js/navigator.webkitPersistentStorage.requestQuota (* size-in-mb 1024 1024)
         (fn [granted-bytes]
-          (!> js/window.webkitRequestFileSystem js/PERSISTENT granted-bytes 
-                                 #(put! c (FileSystem. %))
-                                 (partial my-error-handler "webkitRequestFileSystem" c)))
-        #(put! c [:error %]))
+          (if (>= granted-bytes (* size-in-mb 1024 1024))
+            (!> js/window.webkitRequestFileSystem js/PERSISTENT granted-bytes 
+                #(put! c (FileSystem. %))
+                (partial my-error-handler "webkitRequestFileSystem" c))
+            (do 
+              (.log js/console "requestQuota" "not enough granted bytes")
+              (close! c))))
+        (partial my-error-handler "webkitRequestFileSystem" c))
     c))
-
-
-
--- src/cljs/klozzer/protocols.cljs
+++ src/cljs/klozzer/protocols.cljs
@@ -3,7 +3,7 @@
 
 (defprotocol IFileSystem
   (-write [this filename data] "Writes a file with data")
-  (-file-entry [this filename] "Retrieves the fileEntry javascript object")
+  (-file-entry [this filename create?] "Retrieves the fileEntry javascript object")
   (-file [this filename] "Retrieves the file javascript object")
   (-url [this filename] "Retrieves the url of the file")
   (-read [this filename format] "Reads a file"))
